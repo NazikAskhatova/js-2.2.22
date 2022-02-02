00e0d1fbcc09e6a62b064a4af1f2bf3d94648820
wiusxt <ashatovanas65@gmail.com>
2022-02-02 15:58:48 +0600

    examples added (2)

--- script.js
+++ script.js
@@ -1,105 +1,190 @@
-// variables
-var exampleVariable;
-var message = "hello w";
-var lastName, fisrtName = "Imashev";
-var 
-// number - 2 * * 53 - 1
-// Javascript -  математические операции не ведут к ошибке
-var exampleNumber = 123;
-var exampleNumber2 = 123.45;
-var exampleNumber3 = Infinity; // -Infinity 
-var exampleNumber4 = NaN; // not a number
+// // variables
+// var exampleVariable;
+// var message = "hello w";
+// var lastName, fisrtName = "Imashev";
+// var 
+// // number - 2 * * 53 - 1
+// // Javascript -  математические операции не ведут к ошибке
+// var exampleNumber = 123;
+// var exampleNumber2 = 123.45;
+// var exampleNumber3 = Infinity; // -Infinity 
+// var exampleNumber4 = NaN; // not a number
 
 
-// document.write (typeof exampleNumber4)
+// // document.write (typeof exampleNumber4)
 
-// String
-var exampleString = "Hello world";
-var exampleString2 = 'Hello world';
-var exampleString3 = `hello w`;
-var exampleString4 = "4"; // считается строкой тк есть "" ; '' ; `` ;
+// // String
+// var exampleString = "Hello world";
+// var exampleString2 = 'Hello world';
+// var exampleString3 = `hello w`;
+// var exampleString4 = "4"; // считается строкой тк есть "" ; '' ; `` ;
 
-// boolean
-var exampleBoolean1 = true;
-var exampleBoolean2 = false;
+// // boolean
+// var exampleBoolean1 = true;
+// var exampleBoolean2 = false;
 
-// special types
- var exampleNull = null; //пустая переменная
- var exampleUnderfined;
- var exampleUnderfined2 = underfined;
+// // special types
+//  var exampleNull = null; //пустая переменная
+//  var exampleUnderfined;
+//  var exampleUnderfined2 = underfined;
 
-//  BigInt - not now
-var exampleBigInt = 151982
+// //  BigInt - not now
+// var exampleBigInt = 151982
 
-// symbol - not now
-var exampleSymbol = Symbol("id");
+// // symbol - not now
+// var exampleSymbol = Symbol("id");
 
-// objects
-var exampleObject1 = {};
-var exampleIbject2 = Math;
-// document.write (typeof exampleObject1);
+// // objects
+// var exampleObject1 = {};
+// var exampleIbject2 = Math;
+// // document.write (typeof exampleObject1);
 
-// Weird stuff
+// // Weird stuff
 
-// Output 
-// alert("hello w")
-// var result = prompt("hello w");
-// var result = confirm("hellow w");
-// document.write("hello w");
+// // Output 
+// // alert("hello w")
+// // var result = prompt("hello w");
+// // var result = confirm("hellow w");
+// // document.write("hello w");
 
-// type conservation
-var exampleTypeConservation1 = 5 + "5";
-var exampleTypeConservation2 = 5 + 5 +"5";
-var exampleTypeConservation3 = false + 2;
-var exampleTypeConservation4 = new String(5);
-var exampleTypeConservation5 = new Number("10");
-var exampleTypeConservation6 = new Boolean("");
+// // type conservation
+// var exampleTypeConservation1 = 5 + "5";
+// var exampleTypeConservation2 = 5 + 5 +"5";
+// var exampleTypeConservation3 = false + 2;
+// var exampleTypeConservation4 = new String(5);
+// var exampleTypeConservation5 = new Number("10");
+// var exampleTypeConservation6 = new Boolean("");
 
-// document.write(exampleTypeConservation2);
-// console.log(exampleTypeConservation2);
+// // document.write(exampleTypeConservation2);
+// // console.log(exampleTypeConservation2);
 
-// Math 
-// Operand x = 5 + 6;
-// Operands: 5, 6;
-// OPerator: +
+// // Math 
+// // Operand x = 5 + 6;
+// // Operands: 5, 6;
+// // OPerator: +
 
-// Unary Operator
-var x = 5;
-x = -x;
-var y = "3";
-y = +y;
+// // Unary Operator
+// var x = 5;
+// x = -x;
+// var y = "3";
+// y = +y;
 
-// Binary operator 
-var x = 1, y = 3;
-y = x + y;
+// // Binary operator 
+// var x = 1, y = 3;
+// y = x + y;
+
+// // 
+// var exampleMath1 = 5 + 6;
+// var exampleMath2 = 5 - 1;
+// var exampleMath3 = 5 / 10;
+// var exampleMath4 = 5 * 5;
+
+// // Remainder
+// var exampleMath5 = 5 % 2; // 1 
+// var exampleMath = 6 % 2; // 0
+
+// // Exponent
+// var exampleMath7 = 2 ** 2;
+
+
+// // fast modify
+// var exampleMath8 = 4;
+// exampleMath8 += 5;
+// exampleMath8 -= 3;// exampleMath8 = exampleMath8 - 3;
+
+
+
+// // increment / decrement
+// var exampleMath9 = 0;
+// exampleMath9++;
+// exampleMath9--;
+// ++exampleMath;
+// --exampleMath;
+
+// // Comparisons
+// // var exampleComparison1 = 6 > 5; true
+// // var exampleComparison2 = 6 < 5; false
+// // var exampleComparison3 = 6 >= 5; true
+// // var exampleComparison4 = 6 <= 5; false
+// // var exampleComparison5 = 6 === 5; false
+// // var exampleComparison6 = 6 == 5; f
+// // var exampleComparison7 = 5 !== 5; false
+// // var exampleComparison8 = "5" == 5; true
+// // var exampleComparison9 = "5" != 5; true 
+
+// // document.write ("5" == 5);
+
+// // If else 
+// // if (5 > 3) {
+// //     document.write("hello world");
+// // }
+// // else {
+// //     document.write("bye w")
+// // }
+
+// // 
+// /*
+// Если чайник кипит
+// Выключить газ
+// Иначе
+// Проверить позже
+// */
+
+// // if (isKettleBoling()) {
+// //     switchGasOff();
+// // }
+// // else {
+// //     checkLater();
+// // }
+
+// var age = prompt("how old ar u?");// alert ("u ar" + age + "years old.")
+// var votingAge = age - 18;
+// if (votingAge === NaN) {
+// alert("wrong input. Numbers only.")
+// }
+// else if (votingAge < 0) {
+// alert("You are too young for vote.")
+// }
+// else {
+// alert("you could-ve vote for " + votingAge + " years" )
+// }
 
 // 
-var exampleMath1 = 5 + 6;
-var exampleMath2 = 5 - 1;
-var exampleMath3 = 5 / 10;
-var exampleMath4 = 5 * 5;
 
-// Remainder
-var exampleMath5 = 5 % 2; // 1 
-var exampleMath = 6 % 2; // 0
-
-// Exponent
-var exampleMath7 = 2 ** 2;
+// var userNumber; 
+// while (userNumber != 13) {
+//     userNumber = prompt("Write a thirteen number.")
+// }
 
 
-// fast modify
-var exampleMath8 = 4;
-exampleMath8 += 5;
-exampleMath8 -= 3;// exampleMath8 = exampleMath8 - 3;
+// var userNumber; 
+// while (userNumber != 3.14) {
+//     userNumber = prompt("Write a number PI?")
+// }
+
+
+// начни с числа 1
+// увеличивать число на единицу
+// пока число меньше или равно 3
+
+
+// for (var index = 1; index <= 3; index++) {
+//     document.write(index);
+// }
+
+var exampleFor = "blssed-cursd";
+for (var index = 10; index >= 3; index--) {
+    document.write(exampleFor [index] + " ");
+}
+
+// Functions
+function saHello(name, timeOfDay);
 
 
 
-// increment / decrement
-var exampleMath9 = 0;
-exampleMath9++;
-exampleMath9--;
-++exampleMath;
---exampleMath;
+
+
+
 
 
 
