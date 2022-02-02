24c6de43c1c21aa6c632a803ff52c6a093f207f5
wiusxt <ashatovanas65@gmail.com>
2022-02-02 14:24:38 +0600

    added examples

---
+++ script.js
@@ -0,0 +1,22 @@
+// variables
+var exampleVariable;
+var message = "hello w";
+var lastName, fisrtName = "Imashev";
+var 
+// number
+// Javascript -  математические операции не ведут к ошибке
+var exampleNumber = 123;
+var exampleNumber2 = 123.45;
+var exampleNumber3 = Infinity; // -Infinity 
+var exampleNumber4 = NaN; // not a number
+
+
+// document.write (typeof exampleNumber4)
+
+// String
+var exampleString = "Hello world";
+var exampleString2 = 'Hello world';
+var exampleString3 = `hello w`;
+var exampleString4 = "4"; // считается строкой тк есть "" ; '' ; `` ;
+
+
