f963f204a8dbcf69737f2572b04dccc22c67fd7c
wiusxt <ashatovanas65@gmail.com>
2022-02-02 14:54:41 +0600

    examples added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var exampleVariable;
 var message = "hello w";
 var lastName, fisrtName = "Imashev";
 var 
-// number
+// number - 2 * * 53 - 1
 // Javascript -  математические операции не ведут к ошибке
 var exampleNumber = 123;
 var exampleNumber2 = 123.45;
@@ -18,5 +18,91 @@ var exampleString = "Hello world";
 var exampleString2 = 'Hello world';
 var exampleString3 = `hello w`;
 var exampleString4 = "4"; // считается строкой тк есть "" ; '' ; `` ;
+
+// boolean
+var exampleBoolean1 = true;
+var exampleBoolean2 = false;
+
+// special types
+ var exampleNull = null; //пустая переменная
+ var exampleUnderfined;
+ var exampleUnderfined2 = underfined;
+
+//  BigInt - not now
+var exampleBigInt = 151982
+
+// symbol - not now
+var exampleSymbol = Symbol("id");
+
+// objects
+var exampleObject1 = {};
+var exampleIbject2 = Math;
+// document.write (typeof exampleObject1);
+
+// Weird stuff
+
+// Output 
+// alert("hello w")
+// var result = prompt("hello w");
+// var result = confirm("hellow w");
+// document.write("hello w");
+
+// type conservation
+var exampleTypeConservation1 = 5 + "5";
+var exampleTypeConservation2 = 5 + 5 +"5";
+var exampleTypeConservation3 = false + 2;
+var exampleTypeConservation4 = new String(5);
+var exampleTypeConservation5 = new Number("10");
+var exampleTypeConservation6 = new Boolean("");
+
+// document.write(exampleTypeConservation2);
+// console.log(exampleTypeConservation2);
+
+// Math 
+// Operand x = 5 + 6;
+// Operands: 5, 6;
+// OPerator: +
+
+// Unary Operator
+var x = 5;
+x = -x;
+var y = "3";
+y = +y;
+
+// Binary operator 
+var x = 1, y = 3;
+y = x + y;
+
+// 
+var exampleMath1 = 5 + 6;
+var exampleMath2 = 5 - 1;
+var exampleMath3 = 5 / 10;
+var exampleMath4 = 5 * 5;
+
+// Remainder
+var exampleMath5 = 5 % 2; // 1 
+var exampleMath = 6 % 2; // 0
+
+// Exponent
+var exampleMath7 = 2 ** 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
 
 
